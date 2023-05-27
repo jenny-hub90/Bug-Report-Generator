d17d3ba342c5e0ea3ace265acde2b2dfcd6912a7
jenny-hub90 <jumbledjenny19@gmail.com>
2023-05-27 17:45:16 +0545

    completed making the hompage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
-      <Login/>
-      <Register/>
+      {/* <Login/>
+      <Register/> */}
+      <Home/>
     </div>
   );
 }
